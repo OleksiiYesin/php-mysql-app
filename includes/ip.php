33592757307627ab47e3f01af54ec78cdfd6c57b
OleksiiYesin <86779533+OleksiiYesin@users.noreply.github.com>
2022-03-16 17:45:48 +0200

    Update ip.php

--- includes/ip.php
+++ includes/ip.php
@@ -1,4 +1,4 @@
 <?php
-    $myip = shell_exec('echo `curl -sL http://169.254.169.254/latest/meta-data/public-ipv4`');
-    echo "<span style='color:black;font-weight:500;font-size:18px;'>$myip</span>";
+echo shell_exec('bash includes/metadata.sh');
 ?>
+
